04620289fe844117ce4a19aaa308ea0d6d391e7b
Maxim Chelushkin <maxim.chelushkin@ubisoft.com>
2024-10-15 16:24:30 +0300

    add: reference to naughty attributes in asmdef add: InstantFollower

---
+++ ProceduralAnimation/InstantFollower.cs
@@ -0,0 +1,36 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+public class InstantFollower : MonoBehaviour
+{
+    [Required]
+    public Transform Target;
+
+    public bool ConstrainX;
+    public bool ConstrainY;
+    public bool ConstrainZ;
+    public bool UseFixedUpdate;
+
+
+    void FixedUpdate()
+    {
+        if (UseFixedUpdate)
+            UpdateInternal();
+    }
+
+    void Update()
+    {
+        if (UseFixedUpdate)
+            return;
+        UpdateInternal();
+    }
+
+    private void UpdateInternal()
+    {
+        transform.position = new Vector3(
+            ConstrainX ? transform.position.x : Target.position.x,
+            ConstrainY ? transform.position.y : Target.position.y,
+            ConstrainZ ? transform.position.z : Target.position.z
+        );
+    }
+}
