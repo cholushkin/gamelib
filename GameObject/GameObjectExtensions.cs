a3b3166dfa799ca4e4f7b1d33eba848005cbf013
Max <cholushkin@gmail.com>
2025-12-09 00:23:02 +0200

    change: bound boxes

--- GameObject/GameObjectExtensions.cs
+++ GameObject/GameObjectExtensions.cs
@@ -12,6 +12,7 @@ namespace GameLib.Alg
     public static class GameObjectExtensions
     {
         #region Transform
+
         public static void DestroyChildren(this Transform transform)
         {
             foreach (Transform children in transform)
@@ -35,7 +36,7 @@ namespace GameLib.Alg
         {
             if (string.IsNullOrEmpty(prefix))
                 return null;
-            
+
             if (recursive)
                 return transform.TraverseDepthFirstPreOrder().FirstOrDefault(child => child.name.StartsWith(prefix, StringComparison.Ordinal));
             return transform.Children().FirstOrDefault(child => child.name.StartsWith(prefix, StringComparison.Ordinal));
@@ -46,22 +47,23 @@ namespace GameLib.Alg
             StringBuilder sb = new StringBuilder();
             Assert.IsNotNull(transform);
             var pointer = transform;
-            
-            if(nesting >= 0) // For the negative nesting we totally omit the path
+
+            if (nesting >= 0) // For the negative nesting we totally omit the path
                 sb.Append(pointer.name);
-            
+
             if (addCoordinate)
                 sb.Append($"[pos:{pointer.transform.position}]");
             if (addSiblingIndex)
                 sb.Append($"[{pointer.GetSiblingIndex()}]");
             if (addHash)
                 sb.Append($"#{pointer.gameObject.GetHashCode()}");
-            
+
             while (pointer.parent != null && --nesting >= 0)
             {
                 pointer = pointer.parent;
-                sb.Insert(0, $"{pointer.name}/");  
-            } 
+                sb.Insert(0, $"{pointer.name}/");
+            }
+
             return sb.ToString();
         }
 
@@ -69,23 +71,59 @@ namespace GameLib.Alg
         {
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.RemoveComponent<SphereCollider>();
-            sphere.transform.localScale = new Vector3(radius,radius,radius);
+            sphere.transform.localScale = new Vector3(radius, radius, radius);
             sphere.transform.position = cur.position;
-            if(parenting)
+            if (parenting)
                 sphere.transform.SetParent(cur, true);
-            if(duration > 0f)
+            if (duration > 0f)
                 Object.Destroy(sphere, duration);
         }
-        
+
         public static void CreateDebugSphere(Vector3 pos, float radius = 0.5f, float duration = -1f)
         {
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.RemoveComponent<SphereCollider>();
-            sphere.transform.localScale = new Vector3(radius,radius,radius);
+            sphere.transform.localScale = Vector3.one * radius * 2f;
             sphere.transform.position = pos;
-            if(duration > 0f)
+            if (duration > 0f)
                 Object.Destroy(sphere, duration);
         }
+        
+        /// Returns the depth of a Transform relative to the scene root or a specified root.
+        public static int GetDepth(this Transform child, Transform root = null)
+        {
+            if (child == null)
+                throw new ArgumentNullException(nameof(child));
+
+            int depth = 0;
+            Transform current = child.parent;
+
+            // If no root is specified, count until the global root (parent == null)
+            if (root == null)
+            {
+                while (current != null)
+                {
+                    depth++;
+                    current = current.parent;
+                }
+
+                return depth;
+            }
+
+            // Root specified: walk up until we reach it
+            while (current != null)
+            {
+                if (current == root)
+                    return depth + 1; // +1 to include the direct relationship to root
+
+                depth++;
+                current = current.parent;
+            }
+
+            // If we exited the loop, root was not found in the hierarchy
+            return -1; // root is not an ancestor
+        }
+
 
         // Depth-First Pre-Order Traversal
         public static IEnumerable<Transform> TraverseDepthFirstPreOrder(this Transform transform)
@@ -155,10 +193,12 @@ namespace GameLib.Alg
             foreach (Transform c in t)
                 yield return c;
         }
+
         #endregion
 
         #region Component
-        public static  T CopyComponent<T>(this GameObject destination, T original, bool deep = true) where T : Component
+
+        public static T CopyComponent<T>(this GameObject destination, T original, bool deep = true) where T : Component
         {
             BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic
                                                      | BindingFlags.Instance | BindingFlags.Default | (deep ? 0 : BindingFlags.DeclaredOnly);
@@ -179,9 +219,12 @@ namespace GameLib.Alg
                     {
                         pinfo.SetValue(copy, pinfo.GetValue(original, null), null);
                     }
-                    catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+                    catch
+                    {
+                    } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
                 }
             }
+
             return copy as T;
         }
 
@@ -190,7 +233,7 @@ namespace GameLib.Alg
             if (obj == null)
                 return null;
             var oldComp = obj.GetComponent<T>();
-            if(oldComp)
+            if (oldComp)
                 return oldComp;
             return obj.AddComponent<T>();
         }
@@ -213,7 +256,7 @@ namespace GameLib.Alg
             T t = obj.GetComponent<T>();
             if (null == t)
                 return false;
-            if(immediate)
+            if (immediate)
                 Object.DestroyImmediate(t);
             else
                 Object.Destroy(t);
@@ -232,30 +275,107 @@ namespace GameLib.Alg
                 if (comp != null)
                     return comp;
             } while (cur.parent != null);
+
             return null;
         }
+
         #endregion
 
-        #region Misc
+        #region Bounding boxes
+
+        /// World-space AABB of all renderers under this GameObject (Unity-style).
         public static Bounds BoundBox(this GameObject root)
         {
             Bounds bounds = new Bounds();
-            bool first = true; // could not make it work with nullable
-            var rrs = root.GetComponentsInChildren<Renderer>();
-            foreach (var part in rrs)
+            bool first = true;
+            var renderers = root.GetComponentsInChildren<Renderer>();
+            foreach (var part in renderers)
             {
-                if (!part.gameObject.activeSelf)
+                if (!part.gameObject.activeInHierarchy)
                     continue;
+
                 if (!first)
-                    bounds.Encapsulate(part.bounds);
+                    bounds.Encapsulate(part.bounds); // world-space
                 else
                 {
                     bounds = part.bounds;
                     first = false;
                 }
             }
+
             return bounds;
         }
+
+        /// Axis-aligned bounds in the root's local space (respects root rotation/scale).
+        public static Bounds LocalBoundBox(this GameObject root)
+        {
+            var t = root.transform;
+            return ComputeRelativeBounds(root, t.worldToLocalMatrix);
+        }
+
+        /// Axis-aligned bounds in arbitrary reference space (e.g. grid, parent, etc).
+        /// The returned Bounds is expressed in referenceSpace.local coordinates.
+        public static Bounds BoundBoxRelativeTo(this GameObject root, Transform referenceSpace)
+        {
+            if (referenceSpace == null)
+                return new Bounds(); // empty
+
+            return ComputeRelativeBounds(root, referenceSpace.worldToLocalMatrix);
+        }
+
+        /// Core implementation: computes bounds in the space defined by 'toRef' matrix.
+        /// 'toRef' should be referenceSpace.worldToLocalMatrix.
+        private static Bounds ComputeRelativeBounds(GameObject root, Matrix4x4 toRef)
+        {
+            var renderers = root.GetComponentsInChildren<Renderer>();
+
+            Bounds bounds = new Bounds();
+            bool hasAny = false;
+
+            foreach (var r in renderers)
+            {
+                if (!r.gameObject.activeInHierarchy)
+                    continue;
+
+                Bounds lb = r.localBounds; // in r.local space
+                Matrix4x4 localToWorld = r.localToWorldMatrix;
+                Matrix4x4 localToRef = toRef * localToWorld;
+
+                Vector3 c = lb.center;
+                Vector3 e = lb.extents;
+
+                // 8 corners of the local bounds (r.local space)
+                Vector3[] localCorners =
+                {
+                    new Vector3(c.x - e.x, c.y - e.y, c.z - e.z),
+                    new Vector3(c.x + e.x, c.y - e.y, c.z - e.z),
+                    new Vector3(c.x - e.x, c.y + e.y, c.z - e.z),
+                    new Vector3(c.x + e.x, c.y + e.y, c.z - e.z),
+                    new Vector3(c.x - e.x, c.y - e.y, c.z + e.z),
+                    new Vector3(c.x + e.x, c.y - e.y, c.z + e.z),
+                    new Vector3(c.x - e.x, c.y + e.y, c.z + e.z),
+                    new Vector3(c.x + e.x, c.y + e.y, c.z + e.z),
+                };
+
+                foreach (var lc in localCorners)
+                {
+                    Vector3 p = localToRef.MultiplyPoint3x4(lc); // in reference space
+
+                    if (!hasAny)
+                    {
+                        bounds = new Bounds(p, Vector3.zero);
+                        hasAny = true;
+                    }
+                    else
+                    {
+                        bounds.Encapsulate(p);
+                    }
+                }
+            }
+
+            return bounds;
+        }
+
         #endregion
 
         public static void ForEachChildren(this Transform transform, Action<Transform> func)
@@ -264,13 +384,14 @@ namespace GameLib.Alg
                 func(transform.GetChild(i));
         }
 
-        public static bool ForEachChildrenTo(this Transform transform, Func<Transform,bool> func)
+        public static bool ForEachChildrenTo(this Transform transform, Func<Transform, bool> func)
         {
             for (int i = 0; transform.childCount != i; ++i)
             {
                 if (func(transform.GetChild(i)))
                     return true;
             }
+
             return false;
         }
 
