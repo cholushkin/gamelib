e38f1a13c72c96d7e9249e35384f1bcaf0e1733b
Max <cholushkin@gmail.com>
2026-07-11 19:30:44 +0300

    feat: Hierarchy color tags

---
+++ Editor/Hierarchy/HierarchyIcons.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gamelib
+{
+    [InitializeOnLoad]
+    public static class HierarchyIcons
+    {
+        private static readonly MethodInfo GetIconMethod =
+            typeof(EditorGUIUtility).GetMethod(
+                "GetIconForObject",
+                BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+        static HierarchyIcons()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
+        }
+
+        private static void OnHierarchyGUI(int instanceID, Rect rect)
+        {
+            var go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+            if (go == null)
+                return;
+
+            var icon = GetIconMethod?.Invoke(null, new object[] { go }) as Texture2D;
+            if (icon == null)
+                return;
+
+            // Draw immediately after Unity's built-in GameObject icon.
+            var iconRect = new Rect(
+                rect.x+8,
+                rect.y+8,
+                10,
+                10);
+
+            GUI.DrawTexture(iconRect, icon, ScaleMode.ScaleToFit, true);
+        }
+    }
+}
